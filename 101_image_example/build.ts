813cd43f248d5edd4e76aa1275a9f42cbb6d87bb
Tyler McMullen <tyler@fastly.com>
2018-12-04 23:26:22 -0800

    Include cat.jpg in the deploy script of the image example

--- 101_image_example/build.ts
+++ 101_image_example/build.ts
@@ -2,7 +2,7 @@ import * as gulp from "gulp";
 import { Service, project } from "@wasm/studio-utils";
 
 gulp.task("deploy", async () => {
-  await Service.deployFiles(project.globFiles("src/**"), "c");
+  await Service.deployFiles([...project.globFiles("src/**"), ...project.globFiles("assets/**")], "c");
 });
 
 gulp.task("default", ["deploy"]);
